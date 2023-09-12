aa4350ccd02eba77d23cfb20860a565e8c0332d8
Anthony Gantt <anthony@pcsatease.com>
2023-09-12 15:06:24 -0500

    week one work

--- js/conditions.js
+++ js/conditions.js
@@ -1,5 +1,25 @@
 let flavor = prompt ("Welcome to Codeup Ice Cream! What flavor do you want?");
+/*
+if (flavor === "chocolate") {
+    alert (`One ${flavor}  ice cream coming right up!`)
+} else if (flavor === "vanilla") {
+    alert("One vanilla ic cream coming right up!")
+} else {
+    alert("Sorry, we don't have that flavor")
+}
+*/
 
-if (flavor === "chocoalte) {
-    "alert ("One choclate ice cream coming right up!");
+switch (flavor) {
+    case("chocolate"):
+        alert("One chocolate coming right up!");
+        break;
+    case("vanilla"):
+         alert("One vanilla coming right up!");
+         break;
+    case("strawberry"):
+            alert("One strawberry coming right up!");
+            break;
+    default:
+            alert("We do not have that flavor!");
+            break;
 }
