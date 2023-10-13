a9bd87160fa5b047e09d7b60ee25f2e263f6c385
Anthony Gantt <anthony@pcsatease.com>
2023-10-13 15:55:37 -0500

    add to remote

--- js/BOM exercise.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const delay = 5000; //delay time in milliseconds
-
-const timeoutID = setTimeout(() => alert ('Here is a delayed hello!'), delay);
-
-    //to cancel the timeout, you can call
-    // clearTimeout(timeoutId);
-    // prior to the delay expiring
