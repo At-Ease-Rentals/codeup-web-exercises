1ae53d0a6b5e3f0bdeb4507173c7f6ad5fc1258a
Anthony Gantt <anthony@pcsatease.com>
2023-10-23 01:54:53 -0500

    add html  elements

--- js/objects_date_js.js
+++ js/objects_date_js.js
@@ -22,11 +22,11 @@ let months = [
 // todo:
 // Create the date corresponding to your birthday using the JavaScript Date object.
 //let jsBirthday
-let jsBirthday =new Date (1979, 9, 5);
+let jsBirthday = new Date (1979, 9, 5);
 console.log(jsBirthday)
 
 // todo:
 // Log your birthday in the format: January 1, 2014 using the JavaScript Date object.
 // See link below for methods needed:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#Getter
-console.log('Here is my birthday using JavaScript:' + `${jsBirthday}`);"use strict"
+console.log('Here is my birthday using JavaScript:' + `${jsBirthday}`);
