34f1fbd9582326cc130a322039294413bf8f64db
Anthony Gantt <anthony@pcsatease.com>
2023-10-27 00:48:29 -0500

    updated attribute exercise

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -1,2 +1,29 @@
 "use strict";
 
+// Two seconds after the page loads, change the "profile-pic" src attribute to a different image.
+
+let newProfilePic = setTimeout(updateProfilePic, 2000);
+
+function updateProfilePic() {
+	document.getElementById("profile-pic").src = randomProfilePic();
+}
+
+function randomProfilePic() {
+	return 'https://fer-uig.glitch.me?r=' + Math.random();
+}
+
+
+// Four seconds after the page loads, use innerHTML to change the name in "profile-name".
+
+let newHTML  = setTimeout(updateProfileName, 4000);
+
+function updateProfileName () {
+	let profileName = document.getElementById('profile-name');
+	profileName.innerHTML = "<h2>B. A. Baracus </h2>";
+}
+
+// Six seconds after page loads, add a new class to "profile-desc" that changes the color and font of the description text.
+
+// Use setTimout to create these behaviors.
+
+// Write code that toggles a class on the "profile-card" that changes its background color every two seconds. Use setInterval.
