0a3bb6f7d7ffe111e4686ce78390fc26f265942b
Anthony Gantt <anthony@pcsatease.com>
2023-11-14 14:16:19 -0600

    removed java src files

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const MAPBOX_API_KEY = "pk.eyJ1IjoibWFkbWFyY29zIiwiYSI6ImNsNWw0YWNobTBnY3AzanN4dzdpemhpeG0ifQ.OO7BegDPBkP3SV0CaLbnyQ";
-
-const MOVIE_API_KEY = "http://www.omdbapi.com/?i=tt3896198&apikey=3ebcba35"
