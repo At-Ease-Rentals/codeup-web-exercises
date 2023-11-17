d03dc25ae9a034817fa4c3026c32090e9019d68f
Anthony Gantt <anthony@pcsatease.com>
2023-11-17 14:08:04 -0600

    re-adding missing files

--- js/fetch.js
+++ js/fetch.js
@@ -1,3 +1,49 @@
 "use strict";
 
-fetch(url, {headers:{'Authorization':'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'}})
+fetch("https://api.github.com/users", {headers:{'Authorization':"GITHUB_API"}})
+.then(function(response) {
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+})
+.then(function(json) {
+    console.log(json);
+})
+.catch(function(error) {
+    console.log('There has been a problem with your fetch operation: ' + error.message);
+});
+
+fetch("https://api.github.com/users", {headers:{'Authorization':"GITHUB_API"}})
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+});
+
+
+fetch('data/inventory.json')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
+.catch(error => console.error('Error:', error));
+
+
+
+let table = document.getElementById('myTable'); // replace with your table id
+
+fetch('data/inventory.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach(item => {
+        let row = table.insertRow();
+        let cell1 = row.insertCell();
+        let cell2 = row.insertCell();
+        cell1.textContent = item.name; // replace with your data key
+        cell2.textContent = item.price; // replace with your data key
+    });
+})
+.catch(error => console.error('Error:', error));
+
--- js/http class exercise.js
+++ js/http class exercise.js
@@ -1,27 +1,59 @@
-"use strict"
+"use strict";
+fetch('https://reqres.in/api/users/')
+.then(res => res.json())
+.then(data => console.log(data))
 
-fetch("https://pokeapi.co/api/v2/pokemon/charmander").then(response => {
-    return response.json();
-})
-.then( data => {
-    console.log(data);
-});
+//fetch("https://pokeapi.co/api/v2/pokemon/charmander").then(response => console.log(response));
 
-const myPromise = fetch('https://api.github.com/users');
-myPromise.then(response => console.log(response));
-console.log("See its async!");
-myPromise.catch(error => console.error(error));
+//fetch("https://pokeapi.co/api/v2/pokemon/charmander").then(function(response){
+////    alert("fetch call completed successfully!");
+//    console.log("Response status:" + response.status);
+//    console.log(response.body);
+//    console.log(response.ok);
+//    const data =  response.json();
+//    console.log(data);
+//});
+//
+//fetch("https://pokeapi.co/api/v2/pokemon/charmander").then(function (response) {
+//    return response.json();
+//})
+//.then( function (data) {
+//    console.log(data);
+//});
 
-const githubPromise = fetch('https://api.github.com/repositories');
-const bitbucketPromise = fetch('https://api.bitbucket.org/2.0/repositories');
-Promise.all([githubPromise, bitbucketPromise]).then( data => {
-})
-    .catch( error => {
-});
 
-fetch("https://api.github.com/users").then(response => response.json()).then(users => {
-    users.forEach( user => {
-    console.log(users.login);
-})
-})
-.catch(error => console.error(error));
+
+//const myPromise = fetch('https://api.github.com/users');
+//myPromise.then(response => console.log(response));
+//console.log("See its async!");
+//myPromise.catch(error => console.error(error));
+//
+//const githubPromise = fetch('https://api.github.com/repositories');
+//const bitbucketPromise = fetch('https://api.bitbucket.org/2.0/repositories');
+//Promise.all([githubPromise, bitbucketPromise]).then( data => {
+//})
+//    .catch( error => {
+//});
+//
+//fetch("https://api.github.com/users").then(response => response.json()).then(users => {
+//    users.forEach( user => {
+//    console.log(users.login);
+//})
+//})
+//.catch(error => console.error(error));
+
+
+//const url = "http://worldtimeapi.org/api/timezone/America/New_York";
+//const url = "https://api.thenewsapi.com/v1/news/top?api_token=m4LFWJybesygroQ4vhYq4EoQp5K2ry4pPDLf9T6o";
+//
+//
+//async function getData() {
+//    const request = new Request((url, {
+//        headers: {
+//            'Authorization': ''
+//        }
+//    }))
+//    const response = await fetch(request);
+//    const data = await response.json();
+//    console.log(data);
+//}
